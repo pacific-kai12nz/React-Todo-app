67bfd12a8cf850e58094e0a77be9a21513382de2
kaipora12 <kaipora_12nz@icloud.com>
2025-05-14 01:13:12 +0900

    commit init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import localforage from 'localforage';
+import { isTodos } from "./lib/isTodos";
 
 type Todo ={
   value: string;
@@ -33,6 +35,24 @@ export const App = () => {
     setText('');
   }
 
+  const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+    id: number,
+    key: K,
+    value: V,
+  ) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, [key]: value };
+        } else {
+          return todo;
+        }
+      });
+
+      return newTodos;
+    });
+  };
+
   const handleEdit = (id:number, value:string) => {
       setTodos((todos) => {
         const newTodos = todos.map((todo) => {
@@ -44,6 +64,20 @@ export const App = () => {
         return newTodos;
       });
 };
+
+const handleCheck = (id: number, checked: boolean) => {
+  setTodos((todos) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {...todo, checked};
+      }
+      return todo;
+    });
+
+    return newTodos;
+  });
+};
+
  const handleRemove = (id: number, removed: boolean) => {
   setTodos((todos) => {
     const newTodos = todos.map((todo) => {
@@ -58,6 +92,10 @@ export const App = () => {
 
  const handlerFilter = (filter : Filter) => {
   setFilter(filter);
+ };
+
+ const handleEmpty = () => {
+  setTodos((todos) => todos.filter((todo) => !todo.removed));
  };
 
  const filteredTodos = todos.filter((todo) => {
@@ -75,6 +113,20 @@ export const App = () => {
   }
  });
 
+ useEffect(() => {
+  localforage
+  .getItem("todo-20200101")
+  .then((values) => {
+    if (isTodos(values)) {
+       setTodos(values);
+ }
+});
+ }, []);
+
+ useEffect(() => {
+  localforage.setItem("todo-20200101", todos);
+ }, [todos]);
+
   return (
     <div>
       <select defaultValue="all" onChange={(e) => handlerFilter(e.target.value as Filter)}>
@@ -83,16 +135,25 @@ export const App = () => {
         <option value="unchecked">現在のタスク</option>
         <option value="removed">ゴミ箱</option>
       </select>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input 
-        type='text'
-        value={text}
-        disabled={filter === 'checked' || filter === 're'}
-        onChange={(e) => setText(e.target.value)}
-        />
-        <input type="submit" value={text} onChange={(e) => handleChange(e)} />
-        <input type='submit' value="追加" onSubmit={handleSubmit} />
-      </form>
+      {filter === 'removed' ? (
+        <button onClick={handleEmpty}
+        disabled={todos.filter((todo) => todo.removed).length === 0}
+        >
+          ごみを空にする
+        </button>
+      ) : (
+        filter !== 'checked' && (
+          <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+          >
+            <input type="text" value={text} onChange={(e) => handleChange(e)} />
+            <input type="submit" value="追加" />
+            </form>
+        )
+      )}
       <ul>
         {filteredTodos.map((todo) => {
           return (
@@ -100,15 +161,15 @@ export const App = () => {
               <input 
               type='checkbox'
               checked={todo.checked}
-              onChange={() => handleCheck(todo.id, todo.checked)}
+              onChange={() => handleCheck(todo.id, !todo.checked)}
               />
             <input
             type='text'
-            disabled={todo.checked}
+            disabled={todo.checked || todo.removed}
             value={todo.value}
             onChange={(e) => handleEdit(todo.id, e.target.value)}
             />
-            <button onClick={() => console.log('removed!')}>
+            <button onClick={() => handleRemove(todo.id, !todo.removed)}>
               {todo.removed ? '復元' : '削除'}
             </button>
             </li>
